534aa71580372f4155b5ea4f6c77dc692c5350c9
ahmet <adsayahmet94@hotmail.com>
2023-04-28 18:52:46 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let newWord = []
+  for (let i = (word.length -1); i >= 0 ; i--) {
+    newWord.push(word[i])
+  }
+  let newReversedWord = newWord.join('')
+  return newReversedWord === word ? true : false;
 }
 
 /* 
